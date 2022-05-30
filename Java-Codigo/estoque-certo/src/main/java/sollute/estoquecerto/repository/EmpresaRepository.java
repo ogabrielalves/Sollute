7fa5f801a09e6bd5db9a488e8385d7b83d9e3735
ogabrielalves <gabriel.alves@paygo.com.br>
2022-05-30 10:05:36 -0300

    Correção java.

--- Java-Codigo/estoque-certo/src/main/java/sollute/estoquecerto/repository/EmpresaRepository.java
+++ Java-Codigo/estoque-certo/src/main/java/sollute/estoquecerto/repository/EmpresaRepository.java
@@ -11,16 +11,11 @@ import sollute.estoquecerto.entity.Produto;
 import java.nio.file.Files;
 import java.util.List;
 
-<<<<<<<< HEAD:Java-Codigo/estoque-certo/src/main/java/sollute/estoquecerto/repository/EmpresaRepository.java
 public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
 
-    Empresa findByCnpj(String cnpj);
-========
-@SuppressWarnings("ALL")
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
->>>>>>>> 6ebf1b9a13b7ffd207d44c5b28af6909a5be54bf:Java-Codigo/estoque-certo/estoque-certo/src/main/java/sollute/estoquecerto/repository/EmpresaRepository.java
-
     boolean existsByCnpj(String cnpj);
+
+    Empresa findByCnpj(String cnpj);
 
     @Transactional
     @Modifying
